1fc4194fc6cd372506af21de11b5c35c60b66153
DylanMashini <dylanmashini123@gmail.com>
2022-04-05 13:12:56 -0400

    added www to server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 const isProd = process.env.NODE_ENV == "production";
 
-export default isProd ? "https://dylanmashini.com" : "http://localhost:3000";
+export default isProd
+	? "https://www.dylanmashini.com"
+	: "http://localhost:3000";
