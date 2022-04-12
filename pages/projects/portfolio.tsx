b96c4023ef9905adc3a69b312bdcab517b91405e
DylanMashini <dylanmashini123@gmail.com>
2022-04-12 10:47:42 -0400

    added  construction page

---
+++ pages/projects/portfolio.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import Navbar from "../../components/Navbar";
+
+export default function portfolio() {
+	const isProd = process.env.NODE_ENV == "production";
+	const [mobile, setMobile] = useState(false);
+	const [width, setWidth] = useState(764);
+	const [height, setHeight] = useState(440);
+	const getMobile = () => {
+		setWidth(window.innerWidth);
+		setHeight(window.innerHeight);
+		if (window.innerWidth < 905) {
+			setMobile(true);
+		} else {
+			setMobile(false);
+		}
+	};
+	if (!isProd) {
+		useEffect(() => {
+			getMobile();
+			window.addEventListener("resize", getMobile);
+		});
+	} else {
+		useEffect(() => {
+			getMobile();
+		});
+	}
+	return (
+		<div>
+			<Navbar mobile={mobile} />
+			<div
+				className="project-page-content"
+				style={{ marginTop: "9vh" }}
+				id="0"
+			>
+				<h1>Portfolio Page</h1>
+				<h5>This page is currently under construction.</h5>
+			</div>
+		</div>
+	);
+}
