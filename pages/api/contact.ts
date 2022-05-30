4530f0df88281e87529325c7f921af6d09fe5e46
DylanMashini <dylanmashini123@gmail.com>
2022-05-30 14:54:23 -0400

    rewrite: started portfolio post

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,33 +1,36 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function Contact(req:NextApiRequest, res:NextApiResponse) {
-    console.log("here")
-    if (req.method != "POST") {
-        console.error("bad method")
-        res.status(405).end("Method not allowed");
-        return;
-    }
-    const email = req.body.email;
-    const message = req.body.message;
-    const sgMail = require('@sendgrid/mail')
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
+export default async function Contact(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method != "POST") {
+    console.error("bad method");
+    res.status(405).end("Method not allowed");
+    return;
+  }
+  const email = req.body.email;
+  const message = req.body.message;
+  const sgMail = require("@sendgrid/mail");
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
     to: "dylanmashini123@gmail.com", // Change to your recipient
-    from: 'dylan@dylanmashini.com', // Change to your verified sender
+    from: "dylan@dylanmashini.com", // Change to your verified sender
     subject: `Message from ${email}`,
-    text: 'Message sent from dylanmashini.com',
+    text: "Message sent from dylanmashini.com",
     html: message,
-    }
-    sgMail.send(msg)
+  };
+  sgMail
+    .send(msg)
     .then(() => {
-        console.log('Email sent')
-        res.status(200).json({
-        message: "success"
-    })
+      console.log("Email sent");
+      res.status(200).json({
+        message: "success",
+      });
     })
     .catch((error) => {
-        console.error(error)
-        res.status(200).json({error: error})
-    })
-    
-}  
+      console.error(error);
+      res.status(200).json({ error: error });
+    });
+}
+
