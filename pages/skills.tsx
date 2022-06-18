9e955aa9ae7b80353c4ec6ec8ad28d061e292dee
DylanMashini <dylanmashini123@gmail.com>
2022-06-18 15:51:34 -0400

    rewrite: update skills

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -32,7 +32,7 @@ export default function Skills({ svg }) {
 
 export async function getStaticProps() {
   const svg = await fetch(
-    "https://skillicons.dev/icons?theme=dark&perline=5&i=ts,js,rust,wasm,python,react,figma,mongodb,nextjs,vim"
+    "https://skillicons.dev/icons?theme=dark&perline=5&i=ts,js,rust,wasm,python,react,figma,mongodb,nextjs,vscode"
   ).then((res) => res.text());
   return { props: { svg: svg } };
 }
