28bd42546d6b21d23974dc62d7069e26c7032fc6
DylanMashini <dylanmashini123@gmail.com>
2022-07-11 16:10:34 -0400

    feat: remove prism

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-	enabled: process.env.ANALYZE === "true",
+	enabled:
+		process.env.ANALYZE === "true" && process.env.NODE_ENV != "development",
 });
 const withPreact = require("next-plugin-preact");
 
