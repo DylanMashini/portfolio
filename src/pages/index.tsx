c0dade56940d242adc6a4090751da405d5e10ae5
DylanMashini <54782808+DylanMashini@users.noreply.github.com>
2022-08-23 14:39:46 +0000

    feat: use tailwind for homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,14 +26,7 @@ export default function Home() {
 	return (
 		<Layout mobile={mobile}>
 			<div>
-				<div
-					style={{
-						position: "relative",
-						minHeight: "100vh",
-						minWidth: "100%",
-						clear: "both",
-					}}
-				>
+				<div className="relative min-h-screen clear-both min-w-screen">
 					{mobile ? (
 						<div>
 							<OrangeEclipseMobile />
@@ -51,31 +44,13 @@ export default function Home() {
 					)}
 
 					<div
-						className="glass"
+						className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-45%] h-[80vh] w-[90vw] rounded-2xl backdrop-blur-2xl"
 						style={{
-							position: "absolute",
-							top: "50%",
-							left: "50%",
-							transform: "translate(-50%, -45%)",
-							height: "80vh",
-							width: "90%",
-							borderRadius: "20px",
 							background:
 								"linear-gradient(116.53deg, rgba(255, 255, 255, 0.49) -0.86%, rgba(255, 255, 255, 0.07) 100%)",
-							backdropFilter: "blur(30px)",
 						}}
 					>
-						<div
-							style={{
-								// position: "relative",
-								margin: "0",
-								position: "absolute",
-								top: "50%",
-								left: "50%",
-								transform: "translate(-50%, -50%)",
-								textAlign: "center",
-							}}
-						>
+						<div className="m-0 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-center">
 							<h1 className="fade">Hi, I'm Dylan</h1>
 							<h3 className="fade4">
 								{mobile
@@ -83,15 +58,18 @@ export default function Home() {
 									: "I'm a full stack software developer based in Atlanta."}
 							</h3>
 						</div>
-						<div className={"down-arrow fade"}>
+						<div
+							className={
+								"absolute h-1/5 w-full text-center bottom-0.5 fade"
+							}
+						>
 							<h3>Scroll Down to see more</h3>
 							<DownArrow></DownArrow>
 						</div>
 					</div>
 				</div>
 				<div>
-					<div className={"bio"}>
-						{/* <Bio /> */}
+					<div className="relative mt-[10vh] mb-[10vh]">
 						<Contact mobile={mobile} />
 					</div>
 				</div>
