2583179f7af1ad4a5f6dafabf2765e3eaf14be76
DylanMashini <54782808+DylanMashini@users.noreply.github.com>
2022-10-02 17:34:58 -0400

    Fix: Spelling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,28 @@
 import Carbonbadge from "react-carbonbadge";
 
 export default function Footer({ mobile }: { mobile: boolean }) {
-	return (
-		<div className="footer">
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-					width: "100vw",
-					height: !mobile ? "4.5rem" : "7rem",
-					flexDirection: mobile ? "column" : "row",
-				}}
-			>
-				<h5
-					style={{
-						marginBottom: "1.5em",
-						marginRight: "1em",
-					}}
-				>
-					This site is <a href="/sustainability">made sustainibly</a>{" "}
-				</h5>
-				<Carbonbadge darkMode />
-			</div>
-		</div>
-	);
+  return (
+    <div className="footer">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100vw",
+          height: !mobile ? "4.5rem" : "7rem",
+          flexDirection: mobile ? "column" : "row",
+        }}
+      >
+        <h5
+          style={{
+            marginBottom: "1.5em",
+            marginRight: "1em",
+          }}
+        >
+          This site is <a href="/sustainability">made sustainably</a>{" "}
+        </h5>
+        <Carbonbadge darkMode />
+      </div>
+    </div>
+  );
 }
