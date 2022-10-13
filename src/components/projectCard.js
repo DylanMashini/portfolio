f60fc0d4d254f8b18f0aaed2b4f2652780ea5d7b
DylanMashini <54782808+DylanMashini@users.noreply.github.com>
2022-10-13 18:31:04 -0400

    Refactor: change order on /projects

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -4,58 +4,53 @@ import { useEffect, useState } from "preact/compat";
 import styles from "../styles/projects.module.css";
 
 export default function ProjectCard({
-	imageClassName,
-	width,
-	tech,
-	project,
-	href,
+  imageClassName,
+  width,
+  tech,
+  project,
+  href,
 }) {
-	const [webp, setWebp] = useState(true);
-	const router = useRouter();
-	useEffect(() => {
-		router.prefetch(href);
-		if (window.document.body.className.includes("no-webp")) setWebp(false);
-	}, []);
+  const [webp, setWebp] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    if (!href.includes("http")) {
+      router.prefetch(href);
+    }
+    if (window.document.body.className.includes("no-webp")) setWebp(false);
+  }, []);
 
-	return (
-		<div>
-			<Grid xs={11} sm={5}>
-				<Card
-					className={`${styles[imageClassName]} ${
-						webp ? styles["webp"] : styles["no-webp"]
-					}`}
-					cover
-					hoverable
-					clickable
-					css={{
-						w: "100%",
-						p: 0,
-						minWidth: `${width - 7 / 2}px`,
-						maxWidth: "90vw",
-					}}
-					onClick={() => {
-						router.push(href);
-					}}
-				>
-					<Card.Header
-						css={{ position: "absolute", zIndex: 1, top: 5 }}
-					>
-						<Col>
-							<Text
-								size={12}
-								weight="bold"
-								transform="uppercase"
-								color="black"
-							>
-								{tech}
-							</Text>
-							<Text h3 color="black">
-								{project}
-							</Text>
-						</Col>
-					</Card.Header>
-				</Card>
-			</Grid>
-		</div>
-	);
+  return (
+    <div>
+      <Grid xs={11} sm={5}>
+        <Card
+          className={`${styles[imageClassName]} ${
+            webp ? styles["webp"] : styles["no-webp"]
+          }`}
+          cover
+          hoverable
+          clickable
+          css={{
+            w: "100%",
+            p: 0,
+            minWidth: `${width - 7 / 2}px`,
+            maxWidth: "90vw",
+          }}
+          onClick={() => {
+            router.push(href);
+          }}
+        >
+          <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+            <Col>
+              <Text size={12} weight="bold" transform="uppercase" color="black">
+                {tech}
+              </Text>
+              <Text h3 color="black">
+                {project}
+              </Text>
+            </Col>
+          </Card.Header>
+        </Card>
+      </Grid>
+    </div>
+  );
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -5,86 +5,87 @@ import { useEffect } from "preact/compat";
 import ProjectCard from "./projectCard";
 
 export default function Projects({ width }) {
-	useEffect(() => {
-		let observer = new IntersectionObserver(function (entries) {
-			entries.forEach(entry => {
-				if (entry.isIntersecting) {
-					entry.target.classList.remove("hidden");
-					entry.target.classList.add("fade");
-				}
-			});
-		});
-		observer.observe(document.querySelector(".projectsTitle"));
-	});
-	return (
-		<div
-			style={{
-				width: "100vw",
-				display: "flex",
-				justifyContent: "center",
-			}}
-		>
-			<div
-				id="projects"
-				className="scrollContainer"
-				style={{
-					display: "inline-block",
-					maxWidth: `${width * 2}px`,
-				}}
-			>
-				<h1
-					className="projectsTitle hidden"
-					style={{
-						textAlign: "center",
-						marginTop: "50px",
-					}}
-				>
-					Select Portfolio
-				</h1>
-				<Grid.Container
-					gap={2}
-					justify="center"
-					style={{
-						overflow: "hidden",
-					}}
-				>
-					<ProjectCard
-						imageClassName="boids-card"
-						width={width}
-						tech={"Rust"}
-						project={"3D Boids"}
-						href="/projects/boids"
-					/>
-					<ProjectCard
-						imageClassName="lane-card"
-						width={width}
-						tech="Python"
-						project={"Lane Line Detection"}
-						href="/projects/lane-lines"
-					/>
-					<ProjectCard
-						imageClassName={"ecommerce-card"}
-						width={width}
-						tech="javascript"
-						project="Ecommerce Website"
-						href="projects/ecommerce"
-					/>
-					<ProjectCard
-						imageClassName={"portfolio-card"}
-						width={width}
-						tech={"javascript"}
-						project="Portfolio Page"
-						href="/projects/portfolio"
-					/>
-					<ProjectCard
-						imageClassName={"neat-card"}
-						width={width}
-						tech="python"
-						project="AI Car Game"
-						href="/projects/neat-car-game"
-					/>
-				</Grid.Container>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    let observer = new IntersectionObserver(function (entries) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.remove("hidden");
+          entry.target.classList.add("fade");
+        }
+      });
+    });
+    observer.observe(document.querySelector(".projectsTitle"));
+  });
+  return (
+    <div
+      style={{
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        id="projects"
+        className="scrollContainer"
+        style={{
+          display: "inline-block",
+          maxWidth: `${width * 2}px`,
+        }}
+      >
+        <h1
+          className="projectsTitle hidden"
+          style={{
+            textAlign: "center",
+            marginTop: "50px",
+          }}
+        >
+          Select Portfolio
+        </h1>
+
+        <Grid.Container
+          gap={2}
+          justify="center"
+          style={{
+            overflow: "hidden",
+          }}
+        >
+          <ProjectCard
+            imageClassName={"ecommerce-card"}
+            width={width}
+            tech="javascript"
+            project="Ecommerce Website"
+            href="https://dncnutrition.com/"
+          />
+          <ProjectCard
+            imageClassName={"my-green-earth-card"}
+            width={width}
+            tech={"javascript"}
+            project="Portfolio Page"
+            href="/projects/portfolio"
+          />
+          <ProjectCard
+            imageClassName="boids-card"
+            width={width}
+            tech={"Rust"}
+            project={"3D Boids"}
+            href="/projects/boids"
+          />
+          <ProjectCard
+            imageClassName="lane-card"
+            width={width}
+            tech="Python"
+            project={"Lane Line Detection"}
+            href="/projects/lane-lines"
+          />
+          <ProjectCard
+            imageClassName={"neat-card"}
+            width={width}
+            tech="python"
+            project="AI Car Game"
+            href="/projects/neat-car-game"
+          />
+        </Grid.Container>
+      </div>
+    </div>
+  );
 }
