63d85cccc7653e98166650001642a0e452a05232
DylanMashini <54782808+DylanMashini@users.noreply.github.com>
2022-10-16 10:34:38 -0400

    Feat: Add opengraph meta tag

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,28 +3,29 @@ import Footer from "./Footer";
 import Head from "next/head";
 
 export default function Layout({
-	mobile,
-	children,
-	title = "Dylan Mashini's Portfolio",
-	description = "A Development Portfolio For Dylan Mashini",
+  mobile,
+  children,
+  title = "Dylan Mashini's Portfolio",
+  description = "A Development Portfolio For Dylan Mashini",
 }) {
-	return (
-		<div
-			style={{
-				position: "relative",
-				minHeight: "100vh",
-			}}
-		>
-			<Head>
-				<title>{title}</title>
-				<meta name="description" content={description} />
-			</Head>
-			<div className={`wrapper ${mobile ? "mobile" : ""}`}>
-				<Navbar mobile={mobile} />
-				{children}
-			</div>
+  return (
+    <div
+      style={{
+        position: "relative",
+        minHeight: "100vh",
+      }}
+    >
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:image" content="/api/og" />
+      </Head>
+      <div className={`wrapper ${mobile ? "mobile" : ""}`}>
+        <Navbar mobile={mobile} />
+        {children}
+      </div>
 
-			<Footer mobile={mobile} />
-		</div>
-	);
+      <Footer mobile={mobile} />
+    </div>
+  );
 }
